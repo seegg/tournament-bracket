8d7403a8855ee0a06d7a69a472cb402cd778ab90
yuan tan <dyxtan@gmail.com>
2022-01-07 22:51:22 +1300

    initial commit

---
+++ weback.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['./scripts/index.tsx'],
+  output: {
+    path: path.join(__dirname, 'script'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  },
+  resolves: ['.js', '.jsx', '.tx', '.tsx', '.json'],
+  devtool: 'source-map',
+}
