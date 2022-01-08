8a0e39fa5a1ac5beb185cdd49ac2f3224641a356
yuan tan <dyxtan@gmail.com>
2022-01-08 13:52:22 +1300

    add BracketInput to components

--- scripts/components/App.tsx
+++ scripts/components/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Cell from './Cell'
-
+import BracketInput from './Bracket-Input'
 
 const App = () => {
   return (
     <>
+      <BracketInput />
       <Cell />
     </>
   )
---
+++ scripts/components/Bracket-Input.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BracketInput = () => {
+  return (
+    <div>
+      <label htmlFor="input">Number of participants:</label>
+      <input type="text" name="input" id="" />
+      <button>Go</button>
+    </div>
+  )
+}
+
+export default BracketInput;
