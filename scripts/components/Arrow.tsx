96cc8d710ebb77de3ccc72cc6aab924536f7c91a
yuan tan <dyxtan@gmail.com>
2022-01-10 21:30:01 +1300

    remve reundant styling code from arrow component

--- scripts/components/Arrow.tsx
+++ scripts/components/Arrow.tsx
@@ -8,28 +8,22 @@ const Arrow = ({ position }: ArrowProps) => {
 
   const arrowBorder = 'solid rgb(240, 160, 40) 2px';
   const borderRadius = '0.5rem';
-
-  const justifyStartEnd = {
-    justifyContent: position,
-  };
-
-  const alignSelf = {
-    alignSelf: position
-  };
-
-  const border = {
-    borderRight: arrowBorder,
-    borderTop: position === 'end' ? arrowBorder : '',
-    borderBottom: position === 'end' ? '' : arrowBorder
-  };
-
   const gapFiller = <div style={{ height: '0.5rem' }}></div>;
+
+  const style = {
+    border: {
+      borderRight: arrowBorder,
+      borderTop: position === 'end' ? arrowBorder : '',
+      borderBottom: position === 'end' ? '' : arrowBorder
+    }
+  };
+
 
   return (
     <div className='arrows-container'>
       {position === 'end' && gapFiller}
-      <div className='arrow-divide' style={position === 'start' ? { ...border, borderBottomRightRadius: borderRadius } : {}}></div>
-      <div className='arrow-divide' style={position === 'start' ? {} : { ...border, borderTopRightRadius: borderRadius }}></div>
+      <div className='arrow-divide' style={position === 'start' ? { ...style.border, borderBottomRightRadius: borderRadius } : {}}></div>
+      <div className='arrow-divide' style={position === 'start' ? {} : { ...style.border, borderTopRightRadius: borderRadius }}></div>
       {position === 'start' && gapFiller}
     </div>
   );
