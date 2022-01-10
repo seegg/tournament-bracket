ec464dd773af2c739deab65226f175c8b7be8a58
yuan tan <dyxtan@gmail.com>
2022-01-10 20:54:04 +1300

    move arrow to its own component from cell

--- scripts/components/Arrow.tsx
+++ scripts/components/Arrow.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Arrow from './Arrow';
 
-interface CellProps {
-  position: 'end' | 'start',
-  top?: boolean,
-  win?: boolean,
-  winner?: boolean
-}
+interface ArrowProps {
+  position: 'end' | 'start'
+};
 
-const Cell = ({ position, top }: CellProps) => {
+const Arrow = ({ position }: ArrowProps) => {
 
   const arrowBorder = 'solid rgb(240, 160, 40) 2px';
 
@@ -28,14 +24,14 @@ const Cell = ({ position, top }: CellProps) => {
 
   const gapFiller = <div style={{ height: '0.5rem' }}></div>;
 
-
-
   return (
-    <div className='cell-container' style={justifyStartEnd}>
-      <Arrow position={position} />
-      <div className='cell' style={alignSelf}></div>
+    <div className='arrows-container'>
+      {position === 'end' && gapFiller}
+      <div className='arrow-divide' style={position === 'start' ? border : {}}></div>
+      <div className='arrow-divide' style={position === 'start' ? {} : border}></div>
+      {position === 'start' && gapFiller}
     </div>
   );
-};
+}
 
-export default Cell;
+export default Arrow;
