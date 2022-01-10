734d8c07e4ba2da29b8e02d8ce756a195aa44d26
yuan tan <dyxtan@gmail.com>
2022-01-10 21:25:08 +1300

    move middle divider from matchup to own component

--- scripts/components/Matchup.tsx
+++ scripts/components/Matchup.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Cell from './Cell';
+import MiddleDivider from './MiddleDivider';
 
 const Matchup = () => {
 
@@ -27,16 +28,7 @@ const Matchup = () => {
   return (
     <div className='matchup'>
       <Cell position='end' />
-      <div className='middle-divide'>
-        <div className='arrow-filler' style={style.arrow}>
-          <div style={{ ...style.innerArrow, borderBottom: 'solid rgb(240, 160, 40) 2px' }}></div>
-          <div style={{ ...style.innerArrow, borderTop: 'solid rgb(240, 160, 40) 2px' }}></div>
-        </div>
-        <div style={style.filler}>
-          <div className='middle-filler-top'></div>
-          <div className='middle-filler-bottom'></div>
-        </div>
-      </div>
+      <MiddleDivider />
       <Cell position='start' />
     </div >
   );
---
+++ scripts/components/MiddleDivider.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const MiddleDivider = () => {
+
+  const style = {
+    filler: {
+      display: 'flex',
+      flexDirection: 'column' as 'column',
+      flex: '1 1 auto'
+    },
+
+    arrow: {
+      width: '2rem',
+      display: 'flex',
+      flexDirection: 'column' as 'column',
+    },
+
+    innerArrow: {
+      width: '1.1rem',
+      alignSelf: 'end',
+      flex: '1',
+      borderLeft: 'solid rgb(240, 160, 40) 2px',
+    }
+  };
+
+  return (
+    <div className='middle-divide'>
+      <div className='arrow-filler' style={style.arrow}>
+        <div style={{ ...style.innerArrow, borderBottom: 'solid rgb(240, 160, 40) 2px' }}></div>
+        <div style={{ ...style.innerArrow, borderTop: 'solid rgb(240, 160, 40) 2px' }}></div>
+      </div>
+      <div style={style.filler}>
+        <div className='middle-filler-top'></div>
+        <div className='middle-filler-bottom'></div>
+      </div>
+    </div>);
+}
+
+export default MiddleDivider;
