ac52585095c755e34b7e1ac2a3e89112e13999ac
yuan tan <dyxtan@gmail.com>
2022-01-10 17:15:00 +1300

    move matchup styles into single style object

--- scripts/components/Matchup.tsx
+++ scripts/components/Matchup.tsx
@@ -3,12 +3,24 @@ import Cell from './Cell';
 
 const Matchup = () => {
 
+  const style = {
+    filler: {
+      display: 'flex',
+      flexDirection: 'column' as 'column',
+      flex: '1 1 auto'
+    },
+
+    arrow: {
+      width: '2rem',
+    }
+  }
+
   return (
     <div className='matchup'>
       <Cell position='end' />
       <div className='middle-divide'>
-        <div className='arrow-filler'></div>
-        <div>
+        <div className='arrow-filler' style={style.arrow}></div>
+        <div style={style.filler}>
           <div className='middle-filler-top'></div>
           <div className='middle-filler-bottom'></div>
         </div>
