b88a1d2f92c23cd19d81e42f99dd2a9d17185975
yuan tan <dyxtan@gmail.com>
2022-01-10 21:36:32 +1300

    remove redundant styling code from matchup component

--- scripts/components/Matchup.tsx
+++ scripts/components/Matchup.tsx
@@ -4,27 +4,6 @@ import MiddleDivider from './MiddleDivider';
 
 const Matchup = () => {
 
-  const style = {
-    filler: {
-      display: 'flex',
-      flexDirection: 'column' as 'column',
-      flex: '1 1 auto'
-    },
-
-    arrow: {
-      width: '2rem',
-      display: 'flex',
-      flexDirection: 'column' as 'column',
-    },
-
-    innerArrow: {
-      width: '1.1rem',
-      alignSelf: 'end',
-      flex: '1',
-      borderLeft: 'solid rgb(240, 160, 40) 2px',
-    }
-  }
-
   return (
     <div className='matchup'>
       <Cell position='end' />
