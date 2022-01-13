1e37b00e7a538cde22aaf5691be4b9144a1d1101
yuan tan <dyxtan@gmail.com>
2022-01-14 02:27:27 +1300

    add randomise array of nouns to matchup tree

--- scripts/matchupTree.tsx
+++ scripts/matchupTree.tsx
@@ -1,3 +1,4 @@
+import thing from '../things.json';
 interface Participant {
   name: string,
   id?: number,
@@ -16,14 +17,17 @@ interface Node {
 export class MatchupTree {
   tree: Node[] = [];
   root: Node | null = null;
-  constructor(participantNo: number, matchups: number[]) {
+  constructor(participantNo: number, matchups: number[], names: string[] = thing.things) {
     if (!participantNo || matchups.length <= 0) return;
 
     const totalNodes = matchups.reduce((sum, value) => sum + value * 2, 1);
     // construct the complete tree with empty nodes.
     for (let i = 0; i < totalNodes; i++) {
       let childrenIdx = this.getChildrenIndex(i, totalNodes);
-      this.tree[i] = { value: null, parentIndex: this.getParentIndex(i, totalNodes), leftIndex: childrenIdx[0], rightIndex: childrenIdx[1] };
+      this.tree[i] = {
+        value: null, parentIndex: this.getParentIndex(i, totalNodes),
+        leftIndex: childrenIdx[0], rightIndex: childrenIdx[1]
+      };
     }
 
     //set the root.
@@ -36,7 +40,10 @@ export class MatchupTree {
     let tempID = 1;
     for (let i = 0; i < (matchups[0] - byes) * 2; i++) {
       let nodeValue: Participant = { name: 'Participant ' + tempID, id: tempID, skip: false };
-      this.tree[i] = { value: nodeValue, parentIndex: this.getParentIndex(i, totalNodes), leftIndex: null, rightIndex: null }
+      this.tree[i] = {
+        value: nodeValue, parentIndex: this.getParentIndex(i, totalNodes),
+        leftIndex: null, rightIndex: null
+      }
       tempID++;
     }
 
@@ -45,8 +52,14 @@ export class MatchupTree {
     for (let j = (matchups[0] - byes) * 2; j < matchups[0] * 2; j += 2) {
       let nodeValue: Participant = { name: 'Participant ' + tempID, id: tempID, skip: false };
       let byeValue: Participant = { name: '', skip: true };
-      this.tree[j] = { value: nodeValue, parentIndex: this.getParentIndex(j, totalNodes), leftIndex: null, rightIndex: null };
-      this.tree[j + 1] = { value: byeValue, leftIndex: null, rightIndex: null, parentIndex: this.getParentIndex(j + 1, totalNodes) };
+      this.tree[j] = {
+        value: nodeValue, parentIndex: this.getParentIndex(j, totalNodes),
+        leftIndex: null, rightIndex: null
+      };
+      this.tree[j + 1] = {
+        value: byeValue, leftIndex: null, rightIndex: null,
+        parentIndex: this.getParentIndex(j + 1, totalNodes)
+      };
 
       this.tree[this.getParentIndex(j, totalNodes)!].value = nodeValue;
       tempID++;
@@ -69,3 +82,23 @@ export class MatchupTree {
     return parentIdx < totalNodes ? parentIdx : null;
   }
 }
+
+function shuffle(array: any[]) {
+  let currentIndex = array.length, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+shuffle(thing.things);
