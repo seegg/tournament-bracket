0e12bca865ec9194ba250e5f5e7fa1505eab5f00
yuan tan <dyxtan@gmail.com>
2022-01-15 18:51:45 +1300

    add html for textarea and input toggle

--- scripts/components/Bracket-Input.tsx
+++ scripts/components/Bracket-Input.tsx
@@ -7,6 +7,7 @@ interface InputProps {
 const BracketInput = ({ callback }: InputProps) => {
 
   const [participantNo, setParticipantNo] = useState('');
+  const [isNumberInput, setIsNumberInput] = useState(true);
 
   //matches either a whole number or an empty string.
   const wholeNumberRegex = new RegExp(/(^\d+$)|(^$)/);
@@ -40,6 +41,10 @@ const BracketInput = ({ callback }: InputProps) => {
     }
   }
 
+  const toggleInput = () => {
+    setIsNumberInput(!isNumberInput);
+  }
+
   const style = {
     button: {
       marginLeft: '0.5rem',
@@ -53,13 +58,29 @@ const BracketInput = ({ callback }: InputProps) => {
       maxWidth: '15rem',
       width: '100%',
       flexShrink: '1',
+    },
+    textArea: {
+      marginLeft: '0.5rem',
+      fontSize: '1rem',
+      maxWidth: '15rem',
+      width: '100%'
+    },
+    toggleButton: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: '0.5rem',
+      height: '2rem',
+      width: '2rem'
     }
   };
 
   return (
     <div className='bracket-input'>
-      <button style={style.button} onClick={handleClick}>&#x25BC;</button>
-      <input style={style.input} type="text" placeholder='Number of participants' name="input" id="pariticipant-input" value={participantNo} onChange={handleChange} onKeyPress={handleKeyPress} />
+      <button style={style.toggleButton} onClick={toggleInput}>{isNumberInput ? <>&#9660;</> : <>&#9650;</>}</button>
+      {isNumberInput ? <input style={style.input} type="text" placeholder='Number of participants' name="input" id="pariticipant-input" value={participantNo} onChange={handleChange} onKeyPress={handleKeyPress} /> :
+        <textarea name="" id="" cols={30} rows={10} style={style.textArea} placeholder='Names of participants. comma, space or new line seperated'></textarea>}
+
       <button style={style.button} onClick={handleClick}>Go</button>
     </div>
   )
