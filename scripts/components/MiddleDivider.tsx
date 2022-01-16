9bf1f98b524c95bc1996c740f44d9e94a0aec4ac
yuan tan <dyxtan@gmail.com>
2022-01-16 22:48:36 +1300

    move inner-arrow style from MiddleDivider to style.css

--- scripts/components/MiddleDivider.tsx
+++ scripts/components/MiddleDivider.tsx
@@ -16,20 +16,13 @@ const MiddleDivider = () => {
       display: 'flex',
       flexDirection: 'column' as 'column',
     },
-
-    innerArrow: {
-      width: '1.1rem',
-      alignSelf: 'end',
-      flex: '1',
-      borderLeft: 'solid rgb(240, 160, 40) 2px',
-    }
   };
 
   return (
     <div className='middle-divide'>
       {round != 1 && <div className='arrow-filler' style={style.arrow}>
-        <div style={{ ...style.innerArrow, borderBottom: 'solid rgb(240, 160, 40) 2px', borderBottomLeftRadius: borderRadius }}></div>
-        <div style={{ ...style.innerArrow, borderTop: 'solid rgb(240, 160, 40) 2px', borderTopLeftRadius: borderRadius }}></div>
+        <div className='inner-arrow' style={{ borderBottom: 'solid rgb(240, 160, 40) 2px', borderBottomLeftRadius: borderRadius }}></div>
+        <div className='inner-arrow' style={{ borderTop: 'solid rgb(240, 160, 40) 2px', borderTopLeftRadius: borderRadius }}></div>
       </div>}
       <div style={style.filler}>
         <div className='middle-filler-top'></div>
